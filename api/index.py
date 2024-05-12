73dc1aaee951d30f1df348cb11c0705a92a815ee
simon pineda <simonpineda0521@gmail.com>
2024-05-11 19:39:43 -0500

    The requirements were added

--- api/index.py
+++ api/index.py
@@ -1,24 +1,24 @@
 from flask import Flask, jsonify, request
-import pickle
-import pandas as pd
-from nltk.corpus import stopwords
-from nltk.stem import SnowballStemmer
+# import pickle
+# import pandas as pd
+# from nltk.corpus import stopwords
+# from nltk.stem import SnowballStemmer
 
 
-preprocessor = pickle.load(open('./preprocessor.pkl', 'rb'))
-model = pickle.load(open('./trained_model_XGBoost.pkl', 'rb'))
-def removeStopwordsAndLower(text):
-    stop_words = set(stopwords.words('english')) 
-    words = text.lower().split() 
-    filtered_words = [word for word in words if word not in stop_words] 
-    return ' '.join(filtered_words)
-def lemaAndStem(text):
-    stemmer = SnowballStemmer("english")
-    normalized_text = []
-    for word in text.split():
-        stemmed_word = stemmer.stem(word)
-        normalized_text.append(stemmed_word)
-    return ' '.join(normalized_text).replace(',', '')
+# preprocessor = pickle.load(open('./preprocessor.pkl', 'rb'))
+# model = pickle.load(open('./trained_model_XGBoost.pkl', 'rb'))
+# def removeStopwordsAndLower(text):
+#     stop_words = set(stopwords.words('english')) 
+#     words = text.lower().split() 
+#     filtered_words = [word for word in words if word not in stop_words] 
+#     return ' '.join(filtered_words)
+# def lemaAndStem(text):
+#     stemmer = SnowballStemmer("english")
+#     normalized_text = []
+#     for word in text.split():
+#         stemmed_word = stemmer.stem(word)
+#         normalized_text.append(stemmed_word)
+#     return ' '.join(normalized_text).replace(',', '')
 
 
 
